d425c87c4406eb330125424b5b1de46e2300243b
Artem.Dubovov <artem.dubovov@bk.ru>
2022-05-02 01:41:42 +0300

    feat: finish extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -12,12 +12,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ * => 'STRING PLUS 00 PLUS 00 PLUS **STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  
+  let sep = options.hasOwnProperty('separator') ? String(options.separator) : '+';
+  let addSep = options.hasOwnProperty('additionSeparator') ? String(options.additionSeparator) : '|';
+  let addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  //console.debug(sep, addSep, addition);
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let repeatTimes = options.repeatTimes || 1;
+  let text  = String(str);
+  let substring ;
+  let string ;
+  let res = '';
+  //console.debug(text, sep, addSep, addition, additionRepeatTimes, repeatTimes);
+  for(let i = 0; i < repeatTimes; i++){
+    let string = '';
+    let substr = '';
+    for(let j = 0; j < additionRepeatTimes; j++){
+      if(j == additionRepeatTimes - 1){
+        substr += addition;
+      } else {
+        substr += addition + addSep;
+      }
+      
+    }
+    if(i != 0){
+      string += sep + text + substr ;
+    } else {
+      string += text + substr;
+    }
+    res += string;
+    
+  }
+  //console.debug(res);
+  return res;
 }
 
 module.exports = {
