87482dd39a51b7d7870e42c76aada4190b7d8424
Artem.Dubovov <artem.dubovov@bk.ru>
2022-05-02 02:53:22 +0300

    feat: finish vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true){
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text , key) {
+    if(!text || !key){  
+      throw new Error('Incorrect arguments!');
+    }
+    let cryptoKey = [];
+
+    key.toLowerCase().split('').forEach(el => cryptoKey.push(el.charCodeAt() - 97));
+
+    function cryptoString(str){
+      let newString = '';
+      let index = 0;
+      return str.toLowerCase().split('').map(el => {
+        let elNumber = el.charCodeAt();
+        if(el.charCodeAt() > 122 || el.charCodeAt() < 97){
+          return el;
+        }
+        if(index > cryptoKey.length - 1){
+          index = 0;
+        }
+        if(elNumber + cryptoKey[index] > 122){
+          let res = String.fromCharCode(elNumber + cryptoKey[index] - 26);
+          index++;
+          return res;
+        } else {
+          let res = String.fromCharCode(elNumber + cryptoKey[index]);
+          index++;
+          return res;
+        }
+      })
+    }
+    if(!this.reverse){
+      return cryptoString(text).reverse().join('').toUpperCase();
+    }
+    return cryptoString(text).join('').toUpperCase();
+
+  }
+  decrypt(text, key) {
+    if(!text || !key){  
+      throw new Error('Incorrect arguments!');
+    }
+    let cryptoKey = [];
+
+    key.toLowerCase().split('').forEach(el => cryptoKey.push(el.charCodeAt() - 97));
+
+    function cryptoString(str){
+      let newString = '';
+      let index = 0;
+      return str.toLowerCase().split('').map(el => {
+        let elNumber = el.charCodeAt();
+
+        if(el.charCodeAt() > 122 || el.charCodeAt() < 97){
+          return el;
+        }
+        if(index > cryptoKey.length - 1){
+          index = 0;
+        }
+        if(elNumber - cryptoKey[index] < 97){
+          let res = String.fromCharCode(elNumber - cryptoKey[index] + 26);
+          index++;
+          return res;
+        } else {
+          let res = String.fromCharCode(elNumber - cryptoKey[index]);
+          index++;
+          return res;
+        }
+      })
+    }
+    if(!this.reverse){
+      return cryptoString(text).reverse().join('').toUpperCase();
+    }
+    return cryptoString(text).join('').toUpperCase();
   }
 }
 
